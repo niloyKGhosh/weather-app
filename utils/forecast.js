fc85c86a962729f89564430ad5d3ea1a72b53de8
niloyKGhosh <niloykumar256@gmail.com>
2020-04-22 16:42:12 -0400

    Got the current temperature, feelslike, and description from the api

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,11 +1,34 @@
 const request = require("request");
 
-const url =
-	"http://api.weatherstack.com/current?access_key=dbd6b27f5beee42d81fbd2b91fd82d7d&query=Guelph";
+const forecast = (latitude, longitude, callback) => {
+	const url =
+		"http://api.weatherstack.com/current?access_key=dbd6b27f5beee42d81fbd2b91fd82d7d&query=" +
+		latitude +
+		"," +
+		longitude +
+		"&units=m";
 
-request({ url: url, json: true }, (error, response) => {
-	const actual = response.body.current.temperature;
-	const feelsLike = response.body.current.feelslike;
+	request({ url: url, json: true }, (error, response) => {
+		const actual = response.body.current.temperature;
+		const feelsLike = response.body.current.feelslike;
 
-	console.log(`Actual Temperature: ${actual}°C.\nFeels Like: ${feelsLike}°C`);
+		if (error) {
+			callback("Unable to connect to forecast services", undefined);
+		} else if (response.body.error) {
+			callback(
+				"Location not found. Please try with a different location name.",
+				undefined
+			);
+		} else {
+			callback(undefined, {
+				temperature: response.body.current.temperature,
+				feelsLike: response.body.current.feelslike,
+				description: response.body.current.weather_descriptions,
+			});
+		}
+	});
+};
+
+forecast(43.55, -80.25, (error, data) => {
+	console.log(data);
 });
