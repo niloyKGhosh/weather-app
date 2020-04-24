a499c0a81d6343e5563a4e42db918ebf965ff698
niloyKGhosh <niloykumar256@gmail.com>
2020-04-24 12:03:54 -0400

    Added favicon

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
-const geocode = require("./utils/geocode");
-const forecast = require("./utils/forecast");
+const geocode = require("../utils/geocode");
+const forecast = require("../utils/forecast");
 const express = require("express");
-
+const path = require("path");
 const app = express();
+
+const public = path.join(__dirname, "../public");
+
+app.use(express.static(public));
 
 geocode("guelph", (error, { latitude, longitude, location }) => {
 	if (error) {
@@ -23,4 +27,8 @@ geocode("guelph", (error, { latitude, longitude, location }) => {
 			console.log("Location: ", location);
 		}
 	);
+});
+
+app.listen(3000, () => {
+	console.log("Server is up at port 3000");
 });
