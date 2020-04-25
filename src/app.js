4f5d5f6112d1685b2914f51294d4f411f9603bb7
niloyKGhosh <niloykumar256@gmail.com>
2020-04-25 14:14:19 -0400

    Connected the api with the backend

--- src/app.js
+++ src/app.js
@@ -3,34 +3,43 @@ const forecast = require("../utils/forecast");
 const express = require("express");
 const path = require("path");
 const app = express();
-
 const public = path.join(__dirname, "../public");
 const pathTo404 = path.join(__dirname, "../public/404.html");
+
 app.use(express.static(public));
+
+app.get("/weather", (req, res) => {
+	if (!req.query.address) {
+		return res.send({
+			error: "You must provide an address!",
+		});
+	}
+	geocode(req.query.address, (error, { latitude, longitude, location }) => {
+		if (error) {
+			return res.send({ error });
+		}
+
+		forecast(
+			latitude,
+			longitude,
+			(error, { temperature, feelsLike, description }) => {
+				if (error) {
+					return res.send({ error });
+				}
+
+				res.send({
+					temperature,
+					feelsLike,
+					description,
+					location,
+				});
+			}
+		);
+	});
+});
 
 app.get("*", (req, res) => {
 	res.sendFile(pathTo404);
-});
-
-geocode("guelph", (error, { latitude, longitude, location }) => {
-	if (error) {
-		console.log(error);
-		return;
-	}
-	forecast(
-		latitude,
-		longitude,
-		(error, { temperature, feelsLike, description }) => {
-			if (error) {
-				console.log(error);
-				return;
-			}
-			console.log("Actual: ", temperature);
-			console.log("Feels Like: ", feelsLike);
-			console.log("Description: ", description);
-			console.log("Location: ", location);
-		}
-	);
 });
 
 app.listen(3000, () => {
--- utils/geocode.js
+++ utils/geocode.js
@@ -4,7 +4,7 @@ const geocode = (address, callback) => {
 	const url =
 		"https://api.mapbox.com/geocoding/v5/mapbox.places/" +
 		encodeURIComponent(address) +
-		".json?access_token=pk.eyJ1Ijoibmdob3NoIiwiYSI6ImNrOWJsd2llZTAwamczbGw3MG81YWM3c2kifQ._feULOnc5Z-h7ajIdplLyQ";
+		".json?access_token=pk.eyJ1Ijoibmdob3NoIiwiYSI6ImNrOWJsd2llZTAwamczbGw3MG81YWM3c2kifQ._feULOnc5Z-h7ajIdplLyQ&limit=1";
 
 	request({ url: url, json: true }, (error, { body }) => {
 		if (error) {
