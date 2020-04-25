2e88522a15ccf9992d4eb0a03c711bd051703c42
niloyKGhosh <niloykumar256@gmail.com>
2020-04-25 14:40:18 -0400

    Successfully connected the index.js to the backend

--- public/js/index.js
+++ public/js/index.js
@@ -3,5 +3,15 @@ const weatherForm = document.querySelector("form");
 
 weatherForm.addEventListener("submit", (e) => {
 	e.preventDefault();
-	console.log("testing");
+	const loc = locationInput.value;
+	const address = "http://localhost:3000/weather?address=" + loc;
+	fetch(address).then((response) => {
+		response.json().then((weatherData) => {
+			if (weatherData.error) {
+				console.log(weatherData.error);
+			} else {
+				console.log(weatherData.location, weatherData.temperature);
+			}
+		});
+	});
 });
