c00237ee39e8359bef62cfbf76ffbe3d9d3cca34
niloyKGhosh <niloykumar256@gmail.com>
2020-04-26 10:28:53 -0400

    Started using the weather icon framework

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,32 @@
 const locationInput = document.querySelector("#weatherInput");
 const weatherForm = document.querySelector("form");
+const weatherIcon = document.querySelector(".weather-icon");
+const iconObject = {
+	rain: '<i class="wi wi-rain-mix weatherIcon"></i>',
+	snow: '<i class="wi wi-snow weatherIcon"></i>',
+	sunny: '<i class="wi wi-day-sunny weatherIcon"></i>',
+	cloudy: '<i class="wi wi-cloudy weatherIcon"></i>',
+	haze: '<i class="wi wi-fog weatherIcon"></i>',
+	other: '<i class="wi wi-cloud weatherIcon"></i>',
+};
+
+const selectWeatherIcons = (weatherDescription) => {
+	const desc = weatherDescription.toLowercase();
+
+	if (desc.includes("sun")) {
+		return iconObject.sunny;
+	} else if (desc.includes("rain") || desc.includes("shower")) {
+		return iconObject.rain;
+	} else if (desc.includes("snow")) {
+		return iconObject.snow;
+	} else if (desc.includes("cloud")) {
+		return iconObject.cloudy;
+	} else if (desc.includes("haze") || desc.includes("mist")) {
+		return iconObject.haze;
+	} else {
+		return iconObject.other;
+	}
+};
 
 weatherForm.addEventListener("submit", (e) => {
 	e.preventDefault();
